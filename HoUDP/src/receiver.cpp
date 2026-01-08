bb08768ef44e8fcde8b539a75659dc4fbcc17f92
horolock <horolock96@gmail.com>
2026-01-08 23:06:35 +0900

    Continuously Receiver

--- HoUDP/src/receiver.cpp
+++ HoUDP/src/receiver.cpp
@@ -3,10 +3,22 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <csignal>
+
+volatile bool g_running = true;
+
+void signal_handler(int signum) {
+    std::cout << "\n[!] Caught signal " << signum << ", shutting down..." << std::endl;
+    g_running = false;
+}
 
 int main() {
     const int LISTEN_PORT = 9090;
     const int BUFFER_SIZE = 1024;
+
+    // MARK: - Register signal handler 
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
 
     // MARK: - Create Socket
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -34,29 +46,38 @@ int main() {
     // MARK: - Receive data
     char buffer[BUFFER_SIZE];
     sockaddr_in sender_addr{};
-    socklen_t sender_addr_len = sizeof(sender_addr);
+    socklen_t sender_addr_len;
+    int message_count = 0;
 
-    ssize_t received_bytes = recvfrom(
-        sock, buffer, BUFFER_SIZE - 1, 0, reinterpret_cast<sockaddr*>(&sender_addr), &sender_addr_len
-    );
+    while (g_running) {
+        sender_addr_len = sizeof(sender_addr);
 
-    if (received_bytes < 0) {
-        std::cerr << "Failed to receive data" << std::endl;
-        close(sock);
-        return 1;
+        ssize_t received_bytes = recvfrom(
+            sock,
+            buffer,
+            BUFFER_SIZE - 1,
+            0,
+            reinterpret_cast<sockaddr*>(&sender_addr),
+            &sender_addr_len
+        );
+
+        if (received_bytes < 0) {
+            if (g_running) { std::cerr << "Receive error" << std::endl; }
+            break;
+        }
+
+        buffer[received_bytes] = '\0';
+        message_count++;
+
+        char sender_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &sender_addr.sin_addr, sender_ip, INET_ADDRSTRLEN);
+
+        std::cout << "[#" << message_count << "] " << received_bytes << " bytes from " << sender_ip << ":" << ntohs(sender_addr.sin_port) << " -> \"" << buffer << "\"" << std::endl;    
     }
 
-    buffer[received_bytes] = '\0';
-
-    char sender_ip[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &sender_addr.sin_addr, sender_ip, INET_ADDRSTRLEN);
-
-    std::cout << "[4] Received " << received_bytes << " bytes from " << sender_ip << ":" << ntohs(sender_addr.sin_port) << std::endl;
-    std::cout << "[5] Message: \"" << buffer << "\"" << std::endl;
-
-    // MARK: - Close Socket
     close(sock);
-    std::cout << "[6] Socket Closed" << std::endl;
+
+    std::cout << "[4] Socket closed, Total messages: " << message_count << std::endl;
 
     return 0;
 }
